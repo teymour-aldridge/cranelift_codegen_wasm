73875c2ca771e6a5a80aa8cccc01d2da854ba6df
teymour-aldridge <notifications@reasoning.page>
2022-01-05 18:10:16 +0000

    Tidy up a match arm.

--- src/conversions/cond.rs
+++ src/conversions/cond.rs
@@ -5,17 +5,17 @@ pub(crate) fn wasm_of_cond(cond: IntCC, bits_32: bool) -> BinaryOp {
     match cond {
         IntCC::Equal if bits_32 => BinaryOp::I32Eq,
         IntCC::Equal => BinaryOp::I64Eq,
-        IntCC::NotEqual => todo!(),
-        IntCC::SignedLessThan => todo!(),
-        IntCC::SignedGreaterThanOrEqual => todo!(),
-        IntCC::SignedGreaterThan => todo!(),
-        IntCC::SignedLessThanOrEqual => todo!(),
-        IntCC::UnsignedLessThan => todo!(),
-        IntCC::UnsignedGreaterThanOrEqual => todo!(),
-        IntCC::UnsignedGreaterThan => todo!(),
         IntCC::UnsignedLessThanOrEqual if bits_32 => BinaryOp::I32LeU,
         IntCC::UnsignedLessThanOrEqual => BinaryOp::I64LeU,
-        IntCC::Overflow => todo!(),
-        IntCC::NotOverflow => todo!(),
+        IntCC::NotEqual
+        | IntCC::SignedLessThan
+        | IntCC::SignedGreaterThanOrEqual
+        | IntCC::SignedGreaterThan
+        | IntCC::SignedLessThanOrEqual
+        | IntCC::UnsignedLessThan
+        | IntCC::UnsignedGreaterThanOrEqual
+        | IntCC::UnsignedGreaterThan
+        | IntCC::Overflow
+        | IntCC::NotOverflow => todo!(),
     }
 }
